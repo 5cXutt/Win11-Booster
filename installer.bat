d950137819f4716735bb7110ca5d66ae1f4a7d6b
Scutt <139585282+Scuttlang@users.noreply.github.com>
2023-11-18 20:35:33 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -2,12 +2,12 @@ cls
 color c
 echo "[+] Create a directory on path C:\Users\Booster"
 mkdir C:\Users\Booster
+echo "[+] Directory Created. Press Enter to continue."
 pause
-echo "[+] Directory Created"
 echo "[+] Install Prerequisite for Python"
 powershell -Command "& {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe' -OutFile 'C:\Users\Booster\py.exe' ; Start-Process -FilePath 'C:\Users\Booster\py.exe' -Wait}"
+echo "[+] Python Installed. Press Enter to continue."
 pause
-echo "[+] Python Installed"
 echo "[+] Running booster script"
 python C:\Users\Booster\booster.py
 
