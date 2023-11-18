69f1767f30ba42023b0d3ccb0ad13b4c52db4ed8
Scutt <139585282+Scuttlang@users.noreply.github.com>
2023-11-18 20:29:06 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,4 +1,3 @@
-@echo off
 cls
 color c
 echo "[+] Create a directory on path C:\Users\Booster"
@@ -7,8 +6,5 @@ echo "[+] Directory Created"
 echo "[+] Install Prerequisite for Python"
 powershell -Command "& {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe' -OutFile 'C:\Users\Booster\py.exe' ; Start-Process -FilePath 'C:\Users\Booster\py.exe' -Wait}"
 echo "[+] Python Installed"
-echo "[+] Download Script for booster"
-powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Scuttalng/Win11-Booster/main/boost.py' -OutFile 'C:\Users\Booster\booster.py'}"
-echo "[+] Script for booster downloaded"
 echo "[+] Running booster script"
 python3 C:\Users\Booster\booster.py
