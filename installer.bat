52b4490519e9b7f8cd61a589b50a24c89b859ecc
Scutt <139585282+Scuttlang@users.noreply.github.com>
2023-11-18 20:33:39 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -7,4 +7,6 @@ echo "[+] Install Prerequisite for Python"
 powershell -Command "& {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe' -OutFile 'C:\Users\Booster\py.exe' ; Start-Process -FilePath 'C:\Users\Booster\py.exe' -Wait}"
 echo "[+] Python Installed"
 echo "[+] Running booster script"
-python3 C:\Users\Booster\booster.py
+python C:\Users\Booster\booster.py
+
+pause
