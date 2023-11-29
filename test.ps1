40443b7cc30e0ee73c0f20979fad0ee680927ad1
Scutt <139585282+Scuttlang@users.noreply.github.com>
2023-11-29 19:55:43 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,61 +1,29 @@
-if (-not (Get-Module -Name PSReadLine -ListAvailable)) {
-    Install-Module -Name PSReadLine -Force -Scope CurrentUser
-}
-if (-not (Get-Module -Name PSWindowsUpdate -ListAvailable)) {
-    Install-Module -Name PSWindowsUpdate -Force  
-}
+Add-Type -AssemblyName PresentationFramework
 
-$ScriptPath = $MyInvocation.MyCommand.Path
-$ScriptDirectory = Split-Path $ScriptPath
-Set-Location -Path $ScriptDirectory
+[xml]$xaml = @"
+<Window
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    x:Name="Window" Title="Fortnite Loader" Height="400" Width="600">
+    <Grid>
+        <TextBlock x:Name="txtWelcome" Text="Welcome to Fortnite Loader!" HorizontalAlignment="Center" VerticalAlignment="Center" FontSize="24" />
+        <Button x:Name="btnStart" Content="Start" HorizontalAlignment="Center" VerticalAlignment="Bottom" Margin="0,0,0,20" />
+        <CheckBox x:Name="chkBox" Content="Auto Update" HorizontalAlignment="Center" VerticalAlignment="Bottom" Margin="0,0,0,60" />
+    </Grid>
+</Window>
+"@
+$window = [Windows.Markup.XamlReader]::Load((New-Object -TypeName System.Xml.XmlNodeReader -ArgumentList $xaml))
 
-$logFileName = "log/output.log"
-Start-Transcript -Path $logFileName -Append
+$btnStart = $window.FindName("btnStart")
+$chkBox = $window.FindName("chkBox")
 
-$a = [char]0x2713
-Clear-Host
-$opzioni = @("$a  (Accept Eula)", "X  (Refiute Eula)")
-$opzioneSelezionata = 0
+$btnStart.Add_Click({
+    Write-Host "Start button clicked!"
+})
+$chkBox.Add_Checked({
+})
 
-do {
-Clear-Host
-Write-Host "                                    End-User License Agreement (EULA)   " -ForegroundColor Red 
-Write-Host ""
-Write-Host "            => Please read and accept this agreement to continue using the software" -ForegroundColor DarkMagenta
-Write-Host ""
-Write-Host "            => The creator of this program assumes no responsibility for what may happen" -ForegroundColor DarkMagenta
-Write-Host ""
-Write-Host "            => For help, write to me on Telegram" -ForegroundColor DarkMagenta
-Write-Host ""
-Write-Host "            => To stay updated on the project, visit GitHub => https://github.com/Scuttalng/BoosterV2" -ForegroundColor DarkMagenta
-Write-Host ""
-Write-Host ""
+$chkBox.Add_Unchecked({
+})
 
-    for ($i = 0; $i -lt $opzioni.Count; $i++) {
-        if ($i -eq $opzioneSelezionata) {
-            Write-Host ("[>] " + $opzioni[$i]) -ForegroundColor DarkMagenta 
-        } else {
-            Write-Host ("[ ] " + $opzioni[$i]) -ForegroundColor DarkMagenta
-        }
-    }
-
-    $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
-    switch ($key) {
-        38 { 
-            $opzioneSelezionata = [Math]::Max(0, $opzioneSelezionata - 1)
-        }
-        40 {  
-            $opzioneSelezionata = [Math]::Min($opzioni.Count - 1, $opzioneSelezionata + 1)
-        }
-    }
-} while ($key -ne 13)  
-
-$scelta = $opzioni[$opzioneSelezionata]
-
-if ($scelta -eq "X  (Refiute Eula)") {
-    Write-Host "Hai scelto No.444444"
-    exit
-} else {
-    Write-Host "Hai scelto Sì."
-}
-Stop-Transcript
+$window.ShowDialog() | Out-Null
