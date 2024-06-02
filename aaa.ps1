44a437c1e6028d7cac31d8a9d4c5cde41d82bb9b
Best Cat <139585282+Sqttyxsq@users.noreply.github.com>
2024-06-02 12:12:24 +0200

    Update aaa.ps1

--- aaa.ps1
+++ aaa.ps1
@@ -1,22 +1,13 @@
-# Ottieni il percorso della cartella Desktop
 $desktopPath = [System.Environment]::GetFolderPath('Desktop')
-
-# URL del file da scaricare
 $url = 'https://cdn.discordapp.com/attachments/918148813099786343/1246764624892661770/Nature_Beautiful_short_video_720p_HD.mp4?ex=665d938a&is=665c420a&hm=58e7eaf545f92508ee02eab617099f75e8156f10ae51edbfc61de6d38e4097ef&'  
-
-# Numero di copie da creare
-$numFiles = 80
-
-# Nome originale del file da scaricare
+$numFiles = 50
 $originalFileName = "file.mp4"
 
-# Scarica il file una sola volta
 $downloadedFilePath = Join-Path -Path $desktopPath -ChildPath $originalFileName
 Invoke-WebRequest -Uri $url -OutFile $downloadedFilePath
 
 Write-Host "File scaricato: $downloadedFilePath"
 
-# Crea 50 copie rinominate del file scaricato
 for ($i = 1; $i -le $numFiles; $i++) {
     $newFileName = "file_$i.mp4"
     $newFilePath = Join-Path -Path $desktopPath -ChildPath $newFileName
